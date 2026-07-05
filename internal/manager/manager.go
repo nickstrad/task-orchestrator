6a18c6186ad3022fb35c76592957548d73345647
Nick Strad <nickstrad.dev@gmail.com>
2026-07-05 00:08:56 -0500

    Add manager skeleton

--- internal/manager/manager.go
+++ internal/manager/manager.go
@@ -1,1 +1,20 @@
 package manager
+
+import (
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	"github.com/nickstrad/task-orchestrator/internal/task"
+)
+
+type Manager struct {
+	Pending       queue.Queue
+	TaskDb        map[string][]*task.Task
+	EventDb       map[string][]*task.TaskEvent
+	Workers       []string
+	WorkerTaskMap map[string][]uuid.UUID
+	TaskWorkerMap map[uuid.UUID]string
+}
+
+func (m *Manager) SelectWorker() {}
+func (m *Manager) UpdateTasks()  {}
+func (m *Manager) SendWork()     {}
