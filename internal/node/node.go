fb983d95bcf39f7ef376489057cab8c4f14c733d
Nick Strad <nickstrad.dev@gmail.com>
2026-07-05 00:15:33 -0500

    Add Node skeleton

--- internal/node/node.go
+++ internal/node/node.go
@@ -1,1 +1,13 @@
 package node
+
+type Node struct {
+	Name            string
+	Ip              string
+	Cores           int
+	Memory          int
+	MemoryAllocated int
+	Disk            int
+	DiskAllocated   int
+	role            string
+	TaskCount       int
+}
