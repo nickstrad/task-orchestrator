81c69870b696cb0d2a75875ff196a75b0e0cbc48
Nick Strad <nickstrad.dev@gmail.com>
2026-07-05 00:00:23 -0500

    scaffold woker

--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -1,1 +1,19 @@
 package worker
+
+import (
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	"github.com/nickstrad/task-orchestrator/internal/task"
+)
+
+type Worker struct {
+	Name      string
+	Db        map[uuid.UUID]*task.Task
+	Queue     queue.Queue
+	TaskCount int
+}
+
+func (w *Worker) RunTask()      {}
+func (w *Worker) StartTask()    {}
+func (w *Worker) StopTask()     {}
+func (w *Worker) CollectStats() {}
